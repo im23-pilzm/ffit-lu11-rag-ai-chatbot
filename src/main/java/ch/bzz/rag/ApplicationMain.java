eb76a977a1b68062cee065dbd6d91816da559673
im23-pilzm <pilzm@bzz.ch>
2025-12-09 13:37:42 +0100

    implemented ApplicaitonMain.java

--- src/main/java/ch/bzz/rag/ApplicationMain.java
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -1,12 +1,23 @@
 package ch.bzz.rag;
 
+import ch.bzz.rag.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.MalformedURLException;
 
 @Slf4j
 @SpringBootApplication
 public class ApplicationMain {
+
     public static void main(String[] args) {
-        log.info("Application has been started");
+        WikiPageDownloaderService downloader = new WikiPageDownloaderService();
+        try {
+            downloader.init("https://wiki.bzz.ch");
+            String content = downloader.downloadPage("de:modul:ffit:3-jahr:java:learningunits:lu01:aufgaben:branching");
+            log.info("content: '{}'", content);
+        } catch (MalformedURLException e) {
+            log.error("Error using url {}", e.getMessage(), e);
+        }
     }
 }
