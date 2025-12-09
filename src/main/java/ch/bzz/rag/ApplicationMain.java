a8257c493e22c918a35f0069ec4b4ee4c843e987
im23-pilzm <pilzm@bzz.ch>
2025-12-09 13:59:57 +0100

    Implemented WikiPageCollectorService

--- src/main/java/ch/bzz/rag/ApplicationMain.java
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -2,22 +2,16 @@ package ch.bzz.rag;
 
 import ch.bzz.rag.service.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.MalformedURLException;
+import java.util.Set;
 
 @Slf4j
-@SpringBootApplication
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        WikiPageDownloaderService downloader = new WikiPageDownloaderService();
-        try {
-            downloader.init("https://wiki.bzz.ch");
-            String content = downloader.downloadPage("de:modul:ffit:3-jahr:java:learningunits:lu01:aufgaben:branching");
-            log.info("content: '{}'", content);
-        } catch (MalformedURLException e) {
-            log.error("Error using url {}", e.getMessage(), e);
-        }
+        WikiPageCollectorService collector = new WikiPageCollectorService();
+        String namespace = "de:modul:ffit:3-jahr:java:learningunits:lu11:";
+        Set<String> pages = collector.collectPagesForNamespace("https://wiki.bzz.ch", namespace);
+        log.info(pages.toString());
     }
 }
